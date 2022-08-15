e0bfe7015cb629133f1c8eaa0c30400d2f3245f7
ca3games <ca3games@gmail.com>
2022-08-15 18:25:37 -0500

    Test

--- Calories Survivor/Code/Player/AmmoBasic.gd
+++
@@ -1,9 +0,0 @@
-extends Area2D
-
-export(int) var ammo_stored
-
-
-func _on_AmmoBasic_body_entered(body):
-	if body.is_in_group("PLAYER"):
-		body.ReloadAmmo(ammo_stored)
-		self.queue_free()
--- Calories Survivor/Scenes/Battle/Variables.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-var calories_score = 0
-var steps = 0
-
-onready var GUI = get_tree().get_root().get_node("Battle/GUI")
-
-func AddStep():
-	GUI.AddStep()
